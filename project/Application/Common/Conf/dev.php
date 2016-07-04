1b21cc141579b36756faa7813a2fd1786d60599d
倪亚辉 <nikas1991@hotmail.com>
2016-07-04 22:51:55 +0800

    tm;

--- project/Application/Common/Conf/dev.php
+++ project/Application/Common/Conf/dev.php
@@ -3,17 +3,18 @@ $conf = array(
 	//'配置项'=>'配置值'
     'DEBUG' => true,
     'DB_TYPE' => 'mysql', // 数据库类型
-//    'DB_HOST' => '115.29.235.223', // 服务器地址
-//    'DB_NAME' => 'shopflow', // 数据库名
-//    'DB_USER' => 'admin', // 用户名
-//    'DB_PWD' => 'txg19831210', // 密码
-    'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'shopflow', // 数据库名
-    'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '', // 密码
-    'DB_PORT' => '3306', // 端口
-    'DB_PREFIX' => '', // 数据库表前缀
-    'DB_CHARSET' => 'utf8', // 数据库编码默认采用utf8
+   'DB_HOST' => '115.29.235.223', // 服务器地址
+   'DB_NAME' => 'shopflow', // 数据库名
+   'DB_USER' => 'admin', // 用户名
+   'DB_PWD' => 'txg19831210', // 密码
+
+    // 'DB_HOST' => 'localhost', // 服务器地址
+    // 'DB_NAME' => 'shopflow', // 数据库名
+    // 'DB_USER' => 'root', // 用户名
+    // 'DB_PWD' => '', // 密码
+    // 'DB_PORT' => '3306', // 端口
+    // 'DB_PREFIX' => '', // 数据库表前缀
+    // 'DB_CHARSET' => 'utf8', // 数据库编码默认采用utf8
 
     'URL_CASE_INSENSITIVE' => true,
 
