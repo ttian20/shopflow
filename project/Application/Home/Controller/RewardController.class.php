7eafb659183cb8547c5510897d64901b1329769f
ttian20 <ttian20@gmail.com>
2016-07-20 14:47:19 +0800

    reward

--- project/Application/Home/Controller/RewardController.class.php
+++ project/Application/Home/Controller/RewardController.class.php
@@ -8,25 +8,6 @@ class RewardController extends CommonController {
     }
 
     public function index() {
-        $tasks_mdl = D('Tasks');
-        $tasks_filter = array('passport_id' => $this->_passport['id']);
-        $count = $tasks_mdl->getCount($tasks_filter);
-
-        $utils = new \Common\Lib\Utils();
-        $pagination = $utils->pagination($count, C('PAGE_LIMIT'));
-
-        if (!isset($_GET['p']) || !$_GET['p']) {
-            $page = 1;
-        }
-        else {
-            $page = intval($_GET['p']);
-        }
-        $tasks = $tasks_mdl->getLists($tasks_filter);
-        $this->assign('page', $pagination);
-        $this->assign('tasks', $tasks);
-
-        $taskTypes = $this->_getSearchTypes();
-        $this->assign('taskTypes', $taskTypes);
         $this->display();
     }
 }
